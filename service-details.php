1fd7840567b3135a1cbad6dcd4b8decaafb68290
Evan purification <purificationevan04@gmail.com>
2024-01-20 16:23:54 +0600

    Changing BG, Services details

---
+++ service-details.php
@@ -0,0 +1,114 @@
+<?php include "header.php" ?>
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs">
+      <div class="page-header d-flex align-items-center" style="background-image: url('assets/img/page-header.jpg');">
+        <div class="container position-relative">
+          <div class="row d-flex justify-content-center">
+            <div class="col-lg-6 text-center">
+              <h2>Service Details</h2>
+              <p>Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav>
+        <div class="container">
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Service Details</li>
+          </ol>
+        </div>
+      </nav>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= Service Details Section ======= -->
+    <section id="service-details" class="service-details">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row gy-4">
+
+
+          <div class="col-lg-7">
+            <h3>Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas</h3>
+            <div id="carouselExampleAutoplaying" class="carousel slide my-3" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="assets/img/about.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/img/cargo-service.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/img/logistics-service.jpg" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+           
+            
+            <p>
+              Blanditiis voluptate odit ex error ea sed officiis deserunt. Cupiditate non consequatur et doloremque consequuntur. Accusantium labore reprehenderit error temporibus saepe perferendis fuga doloribus vero. Qui omnis quo sit. Dolorem architecto eum et quos deleniti officia qui.
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circle"></i> <span>Aut eum totam accusantium voluptatem.</span></li>
+              <li><i class="bi bi-check-circle"></i> <span>Assumenda et porro nisi nihil nesciunt voluptatibus.</span></li>
+              <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
+            </ul>
+            <p>
+              Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste veniam repellat odit. Aut pariatur itaque nesciunt fuga.
+            </p>
+            <p>
+              Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum. Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.
+            </p>
+          </div>
+
+          <div class="col-lg-5">
+            <div class="servies-details-payment">
+                <h4 class="dollar">10$</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quibusdam odit incidunt? Eligendi distinctio nihil soluta veritatis expedita aspernatur numquam.</p>
+                <div class="rev-icons">
+                  <p><i class="fa fa-clock"></i> 2 days Delevery </p>
+                  <p><i class="fa fa-refresh"></i> 2 days Delevery </p>
+                </div>
+                <ul class="ser-include">
+                  What's Include? <br>
+                  <li><i class="fa fa-check"></i> 2 Page Website</li>
+                  <li><i class="fa fa-check"></i> 2 Page Website</li>
+                  <li><i class="fa fa-check"></i> 2 Page Website</li>
+                  <li><i class="fa fa-check"></i> 2 Page Website</li>
+                </ul>
+                <div class="buy-now">
+                  <a href="#" >Continue   <i class="fa fa-arrow-right"></i> </a> <br>
+                </div>
+                <a href="#" style="
+                    color: #595959;
+                    display: flex;
+                    justify-content: self-end;
+                    justify-content: space-around; "
+                >Contact </a> <br>
+                
+            
+            </div>
+            
+           
+          </div>
+
+
+
+        </div>
+
+      </div>
+    </section><!-- End Service Details Section -->
+
+  </main><!-- End #main -->
+
+  <?php include "footer.php" ?>
